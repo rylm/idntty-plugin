fbf3ff9ea4a43f116255ad7003e312dcab58be17
Alex Danilov <dai@vt.edu>
2024-06-12 21:40:25 -0600

    Fix ts-expect-error

--- src/app/modules.ts
+++ src/app/modules.ts
@@ -2,7 +2,6 @@
 import { Application } from 'lisk-sdk';
 import { IdentityModule } from './modules/identity/module';
 
-// @ts-expect-error app will have typescript error for unsued variable
 export const registerModules = (app: Application): void => {
     console.log('!!!Modules');
     app.registerModule(new IdentityModule());
