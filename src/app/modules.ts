a6682fa6eb90cba9591bbe0b2e1f3d1301ff0335
root <root@ip-172-26-8-9.eu-central-1.compute.internal>
2024-06-27 05:02:45 +0000

    Add badge module

--- src/app/modules.ts
+++ src/app/modules.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Application } from 'lisk-sdk';
+import { BadgeModule } from "./modules/badge/module";
 import { IdentityModule } from './modules/identity/module';
 
 export const registerModules = (app: Application): void => {
     console.log('!!!Modules');
     app.registerModule(new IdentityModule());
+    app.registerModule(new BadgeModule());
 };
---
+++ src/app/modules/badge/module.ts
@@ -0,0 +1,79 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable @typescript-eslint/member-ordering */
+
+import {
+    BaseModule,
+	ModuleMetadata,
+	// ModuleInitArgs,
+	// InsertAssetContext,
+	// BlockVerifyContext,
+	// TransactionVerifyContext,
+	// VerificationResult,
+	// TransactionExecuteContext,
+	// GenesisBlockExecuteContext,
+	// BlockExecuteContext,
+	// BlockAfterExecuteContext,
+	// VerifyStatus,
+} from 'lisk-sdk';
+import { BadgeEndpoint } from './endpoint';
+import { BadgeMethod } from './method';
+
+export class BadgeModule extends BaseModule {
+    public endpoint = new BadgeEndpoint(this.stores, this.offchainStores);
+    public method = new BadgeMethod(this.stores, this.events);
+    public commands = [];
+
+	// public constructor() {
+	// 	super();
+	// 	// registeration of stores and events
+	// }
+
+	public metadata(): ModuleMetadata {
+		return {
+			...this.baseMetadata(),
+			endpoints: [],
+			assets: [],
+		};
+	}
+
+    // Lifecycle hooks
+    // public async init(_args: ModuleInitArgs): Promise<void> {
+	// 	// initialize this module when starting a node
+	// }
+
+	// public async insertAssets(_context: InsertAssetContext) {
+	// 	// initialize block generation, add asset
+	// }
+
+	// public async verifyAssets(_context: BlockVerifyContext): Promise<void> {
+	// 	// verify block
+	// }
+
+    // Lifecycle hooks
+	// public async verifyTransaction(_context: TransactionVerifyContext): Promise<VerificationResult> {
+		// verify transaction will be called multiple times in the transaction pool
+		// return { status: VerifyStatus.OK };
+	// }
+
+	// public async beforeCommandExecute(_context: TransactionExecuteContext): Promise<void> {
+	// }
+
+	// public async afterCommandExecute(_context: TransactionExecuteContext): Promise<void> {
+
+	// }
+	// public async initGenesisState(_context: GenesisBlockExecuteContext): Promise<void> {
+
+	// }
+
+	// public async finalizeGenesisState(_context: GenesisBlockExecuteContext): Promise<void> {
+
+	// }
+
+	// public async beforeTransactionsExecute(_context: BlockExecuteContext): Promise<void> {
+
+	// }
+
+	// public async afterTransactionsExecute(_context: BlockAfterExecuteContext): Promise<void> {
+
+	// }
+}
