86de961045bddb868bf6b2b8801b1e4eab5e95eb
Alex Danilov <dai@vt.edu>
2024-07-22 22:55:46 -0600

    Add events

---
+++ src/app/modules/identity/events/newInvalidation.ts
@@ -0,0 +1,34 @@
+import { BaseEvent } from 'lisk-sdk';
+
+export interface NewInvalidationEventData {
+    recipientAddress: string;
+    features: {
+        label: string;
+    }[];
+}
+
+export const newInvalidationEventSchema = {
+    $id: 'identity/events/newInvalidation',
+    type: 'object',
+    required: ['recipientAddress', 'features'],
+    properties: {
+        recipientAddress: { fieldNumber: 1, dataType: 'string', minLength: 42, maxLength: 42 },
+        features: {
+            fieldNumber: 2,
+            type: 'array',
+            minItems: 1,
+            maxItems: 16,
+            items: {
+                type: 'object',
+                required: ['label'],
+                properties: {
+                    label: { fieldNumber: 1, dataType: 'string', maxLength: 16 },
+                },
+            },
+        },
+    },
+};
+
+export class NewInvalidationEvent extends BaseEvent<NewInvalidationEventData> {
+    public schema = newInvalidationEventSchema;
+}
