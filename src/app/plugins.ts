82b6de7ca52727cce9193ea3a53a45bb0e8efbd4
Alex Danilov <dai@vt.edu>
2024-07-24 17:53:10 -0600

    Subscribe to network_newBlock

--- src/app/plugins.ts
+++ src/app/plugins.ts
@@ -1,9 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Application } from 'lisk-sdk';
 import { DigitalidentityPlugin } from './plugins/digitalidentity';
-import { EventListenerPlugin } from "./plugins/event_listener/event_listener_plugin";
-
-// import { HelloInfoPlugin } from "./plugins/hello_info/hello_info_plugin";
+import { EventListenerPlugin } from './plugins/event_listener/event_listener_plugin';
 
 export const registerPlugins = (app: Application): void => {
     console.log('!!!plugins');
