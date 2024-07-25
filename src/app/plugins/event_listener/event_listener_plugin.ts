5427e9aa60df8765c6d790ec81cdcb1749a41b4a
Alex Danilov <dai@vt.edu>
2024-07-24 20:41:08 -0600

    Batching & remove relation

--- src/app/plugins/event_listener/event_listener_plugin.ts
+++ src/app/plugins/event_listener/event_listener_plugin.ts
@@ -2,7 +2,7 @@ import { BasePlugin } from 'lisk-sdk';
 import type { BlockJSON } from '@liskhq/lisk-api-client/dist-node/types';
 import { inspect } from 'util';
 
-import { saveNotification } from './database';
+import { saveNotifications } from './database';
 
 export class EventListenerPlugin extends BasePlugin {
     public get nodeModulePath(): string {
@@ -10,20 +10,20 @@ export class EventListenerPlugin extends BasePlugin {
     }
 
     public async load(): Promise<void> {
-        this.apiClient.subscribe('network_newBlock', data => {
+        this.apiClient.subscribe('network_newBlock', async data => {
             const blockData = data as { blockHeader: { id: string } };
-            this.apiClient
-                .invoke('chain_getBlockByID', { id: blockData.blockHeader.id })
-                .then(encodedBlock => {
-                    const block = this.apiClient.block.fromJSON(
-                        encodedBlock as unknown as BlockJSON,
-                    );
-                    console.log('New block:', inspect(block, { depth: null, colors: true }));
-                    block.transactions.forEach(tx => {
-                        saveNotification(tx.senderPublicKey.toString('hex'), tx.command, tx.params);
-                    });
-                })
-                .catch(console.error);
+            const encodedBlock = await this.apiClient.invoke('chain_getBlockByID', {
+                id: blockData.blockHeader.id,
+            });
+            const block = this.apiClient.block.fromJSON(encodedBlock as unknown as BlockJSON);
+            console.log('New block:', inspect(block, { depth: null, colors: true }));
+            await saveNotifications(
+                block.transactions.map(tx => ({
+                    publicKey: tx.senderPublicKey.toString('hex'),
+                    notificationType: tx.command,
+                    data: tx.params,
+                })),
+            );
         });
     }
 
