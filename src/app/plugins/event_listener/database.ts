642ad55e5c9d6b536cc59b6a323c991a0c40bfa5
Alex Danilov <dai@vt.edu>
2024-07-31 21:29:05 -0600

    Update notifications endpoint & schema

--- src/app/plugins/event_listener/database.ts
+++ src/app/plugins/event_listener/database.ts
@@ -4,23 +4,30 @@ const prisma = new PrismaClient();
 
 export interface Notification {
     publicKey: string;
+    forPublicKey?: string;
     notificationType: string;
     data: Record<string, unknown>;
 }
 
 export const saveUserNotifications = async (notifications: Notification[]) =>
     prisma.notification.createMany({
-        data: notifications.map(({ publicKey, notificationType, data }) => ({
+        data: notifications.map(({ publicKey, forPublicKey, notificationType, data }) => ({
             public_key: publicKey,
+            for_public_key: forPublicKey,
             type: notificationType,
             data: JSON.stringify(data),
         })),
     });
 
-export const getUserNotifications = async (publicKey: string, notificationType?: string) =>
+export const getUserNotifications = async (
+    publicKey?: string,
+    forPublicKey?: string,
+    notificationType?: string,
+) =>
     prisma.notification.findMany({
         where: {
             public_key: publicKey,
+            for_public_key: forPublicKey,
             type: notificationType,
         },
     });
