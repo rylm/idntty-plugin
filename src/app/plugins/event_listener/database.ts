0212bdda0766a915fba8d500d0557fce0c78d480
Alex Danilov <dai@vt.edu>
2024-08-14 21:03:45 -0600

    Add transactions table

--- src/app/plugins/event_listener/database.ts
+++ src/app/plugins/event_listener/database.ts
@@ -38,3 +38,52 @@ export const getUserNotifications = async (
             timestamp: 'asc',
         },
     });
+
+export interface Transaction {
+    publicKey: string;
+    forPublicKey?: string;
+    transactionType: string;
+    blockHeight: number;
+    price: bigint;
+    txID: string;
+    data: Record<string, unknown>;
+}
+
+export const saveUserTransactions = async (transactions: Transaction[]) =>
+    prisma.transaction.createMany({
+        data: transactions.map(
+            ({ publicKey, forPublicKey, transactionType, blockHeight, price, txID, data }) => {
+                const timestamp = new Date();
+                return {
+                    public_key: publicKey,
+                    for_public_key: forPublicKey,
+                    type: transactionType,
+                    block_height: blockHeight,
+                    price,
+                    tx_id: txID,
+                    data: JSON.stringify(data),
+                    timestamp,
+                };
+            },
+        ),
+    });
+
+export const getUserTransactions = async (
+    publicKey?: string,
+    forPublicKey?: string,
+    transactionType?: string,
+    blockHeight?: number,
+    txID?: string,
+) =>
+    prisma.transaction.findMany({
+        where: {
+            public_key: publicKey,
+            for_public_key: forPublicKey,
+            type: transactionType,
+            block_height: blockHeight,
+            tx_id: txID,
+        },
+        orderBy: {
+            timestamp: 'asc',
+        },
+    });
