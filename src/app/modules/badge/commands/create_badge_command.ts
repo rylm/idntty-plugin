104fac9330b4554c88a99c6b6420dc25114e6e3e
Alex Danilov <dai@vt.edu>
2024-09-04 05:37:43 +0300

    Temp disable verify #2

--- src/app/modules/badge/commands/create_badge_command.ts
+++ src/app/modules/badge/commands/create_badge_command.ts
@@ -7,7 +7,7 @@ import {
 } from 'lisk-sdk';
 
 import { createBadgeSchema } from '../schema';
-import { IdentityMethod } from '../../identity/method';
+// import { IdentityMethod } from '../../identity/method';
 import { AccountStore } from '../stores/account';
 
 interface Params {
@@ -16,11 +16,11 @@ interface Params {
 
 export class CreateBadgeCommand extends BaseCommand {
     public schema = createBadgeSchema;
-    private _method!: IdentityMethod;
+    // private _method!: IdentityMethod;
 
-    public init(args: { method: IdentityMethod }) {
-        this._method = args.method;
-    }
+    // public init(args: { method: IdentityMethod }) {
+    //     this._method = args.method;
+    // }
 
     public async verify(_context: CommandVerifyContext<Params>): Promise<VerificationResult> {
         // const isAuthority = await this._method.getAccountType(
