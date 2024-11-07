cd9d61070a12d89bb66d60fc428d4f66d82deed3
Alex Danilov <dai@vt.edu>
2024-11-07 14:38:18 +0100

    Event listener transfer

--- src/app/plugins/event_listener/event_listener_plugin.ts
+++ src/app/plugins/event_listener/event_listener_plugin.ts
@@ -1,4 +1,4 @@
-import { BasePlugin } from 'lisk-sdk';
+import { BasePlugin, cryptography } from 'lisk-sdk';
 import type { BlockJSON } from '@liskhq/lisk-api-client/dist-node/types';
 import { inspect } from 'util';
 
@@ -18,19 +18,27 @@ export class EventListenerPlugin extends BasePlugin {
             const block = this.apiClient.block.fromJSON(encodedBlock as unknown as BlockJSON);
             console.log('New block:', inspect(block, { depth: null, colors: true }));
             await saveUserTransactions(
-                block.transactions.map(tx => ({
-                    publicKey: tx.senderPublicKey.toString('hex'),
-                    forPublicKey: ['validateFeature', 'invalidateFeature', 'issueBadge'].includes(
-                        tx.command,
-                    )
-                        ? (tx.params.recipientAddress as string)
-                        : undefined,
-                    transactionType: tx.command,
-                    blockHeight: block.header.height,
-                    price: tx.fee,
-                    txID: tx.id.toString('hex'),
-                    data: tx.params,
-                })),
+                block.transactions.map(tx => {
+                    let forPublicKey: string | undefined;
+                    if (
+                        ['validateFeature', 'invalidateFeature', 'issueBadge'].includes(tx.command)
+                    ) {
+                        forPublicKey = tx.params.recipientAddress as string;
+                    } else if (tx.command === 'transfer') {
+                        forPublicKey = cryptography.address.getLisk32AddressFromAddress(
+                            tx.params.recipientAddress as Buffer,
+                        );
+                    }
+                    return {
+                        publicKey: tx.senderPublicKey.toString('hex'),
+                        forPublicKey,
+                        transactionType: tx.command,
+                        blockHeight: block.header.height,
+                        price: tx.fee,
+                        txID: tx.id.toString('hex'),
+                        data: tx.params,
+                    };
+                }),
             );
             await saveUserNotifications(
                 block.transactions
