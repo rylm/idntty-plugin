6da27d2d52381f280cbc078bb256ef95ea206a1c
Alex Danilov <dai@vt.edu>
2025-01-23 01:54:55 -0600

    New genesis block

--- src/app/plugins.ts
+++ src/app/plugins.ts
@@ -2,13 +2,7 @@
 import { Application } from 'klayr-sdk';
 import { DashboardPlugin } from '@klayr/dashboard-plugin';
 
-import { DigitalidentityPlugin } from './plugins/digitalidentity';
-import { EventListenerPlugin } from './plugins/event_listener/event_listener_plugin';
-
 export const registerPlugins = (app: Application): void => {
     console.log('!!!plugins');
     app.registerPlugin(new DashboardPlugin());
-
-    app.registerPlugin(new DigitalidentityPlugin());
-    app.registerPlugin(new EventListenerPlugin());
 };
