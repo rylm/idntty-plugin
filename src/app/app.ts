1a067e1897c077f306b6f6d50964074ca31f9951
Alex Danilov <dai@vt.edu>
2025-01-23 02:57:00 -0600

    New config & disable modules and plugins

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,20 @@
 import { Application, Types } from 'klayr-sdk';
 // import { registerModules } from './modules';
-import { registerPlugins } from './plugins';
-import { BadgeModule } from './modules/badge/module';
-import { IdentityModule } from './modules/identity/module';
+// import { registerPlugins } from './plugins';
+// import { BadgeModule } from './modules/badge/module';
+// import { IdentityModule } from './modules/identity/module';
 
 export const getApplication = (config: Types.PartialApplicationConfig): Application => {
     const { app } = Application.defaultApplication(config);
-    const badgeModule = new BadgeModule();
-    const identityModule = new IdentityModule();
+    // const badgeModule = new BadgeModule();
+    // const identityModule = new IdentityModule();
 
-    app.registerModule(badgeModule);
-    app.registerModule(identityModule);
-    badgeModule.addDependencies(identityModule.method);
+    // app.registerModule(badgeModule);
+    // app.registerModule(identityModule);
+    // badgeModule.addDependencies(identityModule.method);
 
     // registerModules(app);
-    registerPlugins(app);
+    // registerPlugins(app);
 
     console.log('!!!! app');
 
